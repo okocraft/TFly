e5f462d307d324bb48e2d109306bfb6efb702b48
Siroshun09 <siro.shun8@gmail.com>
2024-04-20 19:02:39 +0900

    build,refactor: update messages to 0.7.0

--- src/main/java/net/okocraft/tfly/TFlyPlugin.java
+++ src/main/java/net/okocraft/tfly/TFlyPlugin.java
@@ -169,13 +169,10 @@ public class TFlyPlugin extends JavaPlugin {
             this.localization.clearSources();
         }
 
-        DirectorySource.forStringMessageMap(this.getDataFolder().toPath().resolve("languages"))
-                .fileExtension(PropertiesFile.FILE_EXTENSION)
+        DirectorySource.propertiesFiles(this.getDataFolder().toPath().resolve("languages"))
                 .defaultLocale(Locale.ENGLISH, Locale.JAPANESE)
-                .messageLoader(PropertiesFile.DEFAULT_LOADER)
-                .messageProcessor(MessageProcessors.appendMissingStringMessages(this::loadDefaultMessageMap, PropertiesFile.DEFAULT_APPENDER))
-                .messageProcessor(loaded -> MiniMessageSource.create(loaded.messageSource()))
-                .load(loaded -> this.localization.addSource(loaded.locale(), loaded.messageSource()));
+                .messageProcessor(MessageProcessors.appendMissingMessagesToPropertiesFile(this::loadDefaultMessageMap))
+                .load(loaded -> this.localization.addSource(loaded.locale(), MiniMessageSource.create(loaded.messageSource())));
     }
 
     private @Nullable Map<String, String> loadDefaultMessageMap(@NotNull Locale locale) throws IOException {
